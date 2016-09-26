8ddfa45fb9ace4dc2538a9f1b8c936a048b251cd
Nhan Cao <nhancv1992@gmail.com>
2016-09-26 11:02:36 +0700

    hello world

--- app/src/main/java/com/nhancv/mosbymvp/MainActivity.java
+++ app/src/main/java/com/nhancv/mosbymvp/MainActivity.java
@@ -1,13 +1,51 @@
 package com.nhancv.mosbymvp;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.hannesdorfmann.mosby.mvp.MvpActivity;
+import com.nhancv.mosbymvp.presenter.HelloWorldPresenter;
+import com.nhancv.mosbymvp.view.HelloWorldView;
+
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends MvpActivity<HelloWorldView, HelloWorldPresenter> implements HelloWorldView {
+
+    @ViewById(R.id.activity_main_tv_greeting)
+    TextView activity_main_tv_greeting;
+
+    @NonNull
+    @Override
+    public HelloWorldPresenter createPresenter() {
+        return new HelloWorldPresenter();
+    }
+
+    @Click(R.id.activity_main_btn_get_hello)
+    public void activity_main_btn_get_hello_on_click() {
+        presenter.greetHello();
+    }
+
+    @Click(R.id.activity_main_btn_get_goodbye)
+    public void activity_main_btn_get_goodbye_on_click() {
+        presenter.greetGoodbye();
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void showHello(String greetingText) {
+        activity_main_tv_greeting.setTextColor(Color.RED);
+        activity_main_tv_greeting.setText(greetingText);
     }
+
+    @Override
+    public void showGoodbye(String greetingText) {
+        activity_main_tv_greeting.setTextColor(Color.BLUE);
+        activity_main_tv_greeting.setText(greetingText);
+    }
+
+
+
 }
